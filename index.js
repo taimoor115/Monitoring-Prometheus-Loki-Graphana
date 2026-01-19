7fd1d34d200d13021a801928023a4a37bba1a659
Taimoor Hussain <taimoorhussain985@gmail.com>
2026-01-19 12:20:53 +0500

    fix: update server port from 3000 to 4000 in API requests

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { simulateSlowResponse, getFastResponse } = require('./utility');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 
